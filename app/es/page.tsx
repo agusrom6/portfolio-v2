6cf04488db468b08846fe0c9f582f574ad79452e
agustina <agustinaromer6@gmail.com>
2026-03-08 23:29:34 -0300

    feat: add internationalization structure for es and en routes

---
+++ app/es/page.tsx
@@ -0,0 +1,33 @@
+
+import { SectionContainer } from "@/common/layout/SectionContainer";
+import { About } from "@/views/About";
+import { Contact } from "@/views/Contact";
+import Experience from "@/views/Experience";
+import { Hero } from "@/views/Hero";
+import { Projects } from "@/views/Projects";
+import { Skills } from "@/views/Skills";
+
+export default function Home() {
+  return (
+    <>
+      <SectionContainer as="section" glow="center">
+        <Hero />
+      </SectionContainer>
+      <SectionContainer glow="top-left">
+        <Experience />
+      </SectionContainer>
+      <SectionContainer glow="top-right" withTopBorder>
+        <Projects />
+      </SectionContainer>
+      <SectionContainer>
+        <Skills />
+      </SectionContainer>
+      <SectionContainer>
+        <About />
+      </SectionContainer>
+      <SectionContainer glow="center" withTopBorder>
+        <Contact />
+      </SectionContainer>
+    </>
+  );
+}
