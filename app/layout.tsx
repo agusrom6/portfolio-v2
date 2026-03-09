46ea667798957764997406d223125241f04f7190
agustina <agustinaromer6@gmail.com>
2026-03-08 23:28:29 -0300

    style: update global layout and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Navbar } from "@/common/layout/Navbar";
+import { Footer } from "@/common/layout/Footer";
+import { TranslationProvider } from "@/common/context/translation-context";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <TranslationProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </TranslationProvider>
       </body>
     </html>
   );
