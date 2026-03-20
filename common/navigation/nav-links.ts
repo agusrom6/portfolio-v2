aeb746f83190dddcba7cdc91d3648e245c866869
agustina <agustinaromer6@gmail.com>
2026-03-20 11:54:34 -0300

    fix(about): update url img

--- common/navigation/nav-links.ts
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-  { key: "home", href: "/" },
-  { key: "projects", href: "#projects" },
-  { key: "contact", href: "#contact" },
-] as const;
-
-export type NavLinkKey = (typeof navLinks)[number]["key"];
